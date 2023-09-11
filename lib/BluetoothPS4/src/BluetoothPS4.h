d23d00b9a30b0100c2e9f3fb43d2309edb07e77a
DaiGuard <zenith.or.w@gmail.com>
2023-09-12 00:43:10 +0900

    Bluetooth PS4 コントローラライブラリの追加

---
+++ lib/BluetoothPS4/src/BluetoothPS4.h
@@ -0,0 +1,19 @@
+#ifndef __BLUETOOTH_PS4_H__
+#define __BLUETOOTH_PS4_H__
+
+
+#include <HID.h>
+
+class BluetoothPS4: public HID
+{
+    public:
+        BluetoothPS4(): HID(){};
+        ~BluetoothPS4(){};
+
+    bool begin();
+    bool isConnected();    
+    void read();
+};
+
+
+#endif
