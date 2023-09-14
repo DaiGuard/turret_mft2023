ce245a3fd608171bd9e48ae1af9f0f49f0b68c76
DaiGuard <zenith.or.w@gmail.com>
2023-09-14 23:09:14 +0900

    fire control library add

---
+++ lib/FireControl/src/FireControl.h
@@ -0,0 +1,43 @@
+#ifndef __FIRE_CONTROL_H__
+#define __FIRE_CONTROL_H__
+
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <IRrecv.h>
+#include <stdint.h>
+
+
+class FireControl:
+    public IRrecv, IRsend
+{
+    public:
+        FireControl(int send_pin, int recv_pin, uint8_t no);
+        ~FireControl(){}
+
+        bool begin();
+
+        void fire();
+        int damage();
+
+    private:
+        int _no;
+        
+        const uint16_t _DATA_HIGH;
+        const uint16_t _DATA_LOW;        
+
+        uint16_t _rawData[67] =
+            {
+                9000, 4500, 650, // header
+                1650, 600, 550, 650, 1650, 600, 550, 600, // 0xA
+                550, 600, 1650, 650, 550, 600, 1650, 650, // 0x5
+                550, 600, 550, 650, 550, 600, 550, 600,   // 0x0
+                550, 600, 550, 650, 550, 600, 550, 600,   // 0x0            
+                550, 600, 550, 650, 550, 600, 550, 600,    // 0x0
+                550, 600, 550, 650, 550, 600, 550, 600,     // 0x0
+                550, 600, 550, 650, 550, 600, 550, 600,  // 0x0
+                550, 600, 550, 650, 550, 600, 550, 600  // 0x0
+            };
+};
+
+
+#endif
---
+++ lib/Speaker/README.md
@@ -0,0 +1,13 @@
+# Speaker class
+
+---
+
+## Create sound raw data
+
+```bash
+xxd -i data.raw > data.txt
+```
+
+## ToDo
+
+[ ] Save sound data to flash memory
